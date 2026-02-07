09ec774284da5173eae9e21ab10cec62cb1dee14
Kaka <suprekaka@outlook.com>
2026-02-07 18:04:17 +0800

    sync

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,47 @@
+import { useState } from 'react'
 import { View, ScrollView } from '@tarojs/components'
-import { Grid, GridItem, Image } from '@nutui/nutui-react-taro'
-import CustomTabBar from '../../components/CustomTabBar';
+import Taro from '@tarojs/taro'
+import { Grid, GridItem, Image, FixedNav } from '@nutui/nutui-react-taro'
+import CustomTabBar from '../../components/CustomTabBar'
 import './index.scss'
 
 // 这里的图片建议替换成你真实的竖屏视频封面
 const imgSrc = 'https://m.360buyimg.com/babel/jfs/t1/36973/29/11270/120042/5cf1fe3cEac2b5898/10c2722d0cc0bfa7.png'
 
 function Home() {
+  const debugList = [
+    {
+      id: 1,
+      text: '首页',
+      icon: '',
+      path: 'pages/home/index'
+    },
+    {
+      id: 2,
+      text: '声纹采集',
+      icon: '',
+      path: 'pages/voiceCollector/index'
+    },
+    {
+      id: 3,
+      text: '播放页',
+      icon: '',
+      path: 'pages/player/index'
+    },
+  ]
+  const [visible, setVisible] = useState(false)
+  const change = (value: boolean) => {
+    setVisible(value)
+  }
+  const selected = (
+    item: any,
+    event: React.MouseEvent<Element, MouseEvent>
+  ) => {
+    console.log(item, event)
+    Taro.navigateTo({ url: `/${item.path}` })
+  }
+
+
   const renderList = () => {
     const list = Array(17).fill(imgSrc)
     return (
@@ -44,6 +79,16 @@ function Home() {
 
       <CustomTabBar />
       <View className='header-bg' />
+
+      <FixedNav
+        list={debugList}
+        inactiveText='DEV'
+        // overlay
+        position={{ top: '520px' }}
+        onChange={change}
+        visible={visible}
+        onSelect={selected}
+      />
     </View>
   )
 }
