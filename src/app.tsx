f7ce998b020e34fff309f973c47610f7d0ada5e3
Kaka <suprekaka@outlook.com>
2026-02-09 20:47:50 +0800

    sync

--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,24 @@
-import React, { useEffect } from 'react'
-import Taro, { useDidShow, useDidHide } from '@tarojs/taro'
+import React  from 'react'
+import Taro, { useDidShow, useDidHide, useLaunch } from '@tarojs/taro'
 import { View } from '@tarojs/components';
 // 全局样式
 import './app.scss'
 
 function App(props: React.PropsWithChildren) {
   const { children } = props
-  // 可以使用所有的 React Hooks
-  useEffect(() => {
-    (async () => {
-      const { code } = await Taro.login()
-      console.log('~~~~~~~ code', code);
-    })()
-  }, []);
+
+  // 小程序初始化时触发
+  useLaunch(async () => {
+    // 初始化微信云环境
+    Taro.cloud.init({
+      env: 'cloud1-3gfloa3s868e6640', // 替换为你的云环境ID
+      traceUser: true, // 记录用户访问
+    })
+
+    // 登录
+    const { code } = await Taro.login()
+    console.log('~~~~~~~ code', code);
+  });
 
   // 对应 onShow
   useDidShow(() => {})
@@ -20,11 +26,6 @@ function App(props: React.PropsWithChildren) {
   // 对应 onHide
   useDidHide(() => {})
 
-  // 初始化微信云环境
-  Taro.cloud.init({
-    env: 'cloud1-3gfloa3s868e6640', // 替换为你的云环境ID
-    traceUser: true, // 记录用户访问
-  })
 
   return (
     <View>
