34f69a19c28fe7ff025c73b081e63970c99beb58
Kaka <suprekaka@outlook.com>
2026-02-10 11:51:52 +0800

    sync

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,7 +1,7 @@
 export default defineAppConfig({
   pages: [
-    'pages/player/index',
     'pages/home/index',
+    'pages/player/index',
     'pages/voiceCollector/index',
     'pages/videoCreator/index',
     'pages/mine/index',
