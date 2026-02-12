67cbb4fea72bb16a0b5ff5db4cc13dc5eee6cc21
Kaka <suprekaka@outlook.com>
2026-02-12 15:40:35 +0800

    用 async 函数重构代码

--- miniprogram/src/pages/videoCreator/index.tsx
+++ miniprogram/src/pages/videoCreator/index.tsx
@@ -4,6 +4,7 @@ import { Uploader, TextArea, Button } from '@nutui/nutui-react-taro';
 import Taro from '@tarojs/taro';
 import CustomTabBar from '../../components/CustomTabBar';
 import './index.scss';
+import {after} from 'node:test';
 
 const VideoCreator: React.FC = () => {
   const [currentFiles, setCurrentFiles] = useState<any[]>([]);
@@ -14,8 +15,140 @@ const VideoCreator: React.FC = () => {
 
 
   const handleUploadChange = (data: { fileList: any[] }) => {
-    setCurrentFiles(data.fileList);
+    setCurrentFiles(data.fileList)
   };
+
+  /**
+   * step1 上传图片到微信云存储，并返回图片公网 URL
+   * @param cloudPath
+   * @param imageUrl
+   */
+  const uploadImageToWxCloud = async (cloudPath: string, imageUrl: string): Promise<string> => {
+    try {
+      const { fileID } = await Taro.cloud.uploadFile({
+        cloudPath: cloudPath,
+        filePath: imageUrl,
+      })
+      await Taro.showToast({ title: '上传成功', icon: 'success' })
+      // setTimeout(() => setDuration(0), 1000);
+
+      const fileURLRes = await Taro.cloud.getTempFileURL({
+        fileList: [fileID]
+      })
+
+      const publicImagUrl = fileURLRes.fileList[0].tempFileURL;
+      console.log('uploaded public image url: ' + publicImagUrl)
+      return publicImagUrl;
+    } catch(err) {
+      console.error(err)
+      await Taro.showToast({ title: '上传失败', icon: 'error' })
+    }
+  }
+
+  /**
+   * 读取当前用户的 voiceId
+   */
+  const fetchVoiceId = async (): Promise<string> => {
+    // 查询当前用户的voice_id
+    const userVoiceRes = await Taro.cloud.database().collection('user_voice')
+      .where({
+        'user_id': '123456'
+      })
+      .get()
+    const voiceId = userVoiceRes.data[0].voice_id
+    console.log('voiceId: ' + voiceId)
+    return voiceId
+  }
+
+  /**
+   * step 2：根据祝福文本+之前的音色，调用大模型合成声音
+   * @param cloudPath
+   * @param imageUrl
+   */
+  const generateAudio = async () => {
+    // https://help.aliyun.com/zh/model-studio/cosyvoice-clone-api
+    /**
+     * input:
+     *  voice_id: voiceId
+     *  text:  blessingText
+     *
+     * ouput:
+     *  audioPublicUrl
+     *
+     * API: https://dashscope.aliyuncs.com/api/v1/services/aigc/text2audio/text-to-audio
+     *         {
+       method: 'POST',
+       headers: {
+         'Authorization': `Bearer ${this.apiKey}`,
+         'Content-Type': 'application/json',
+       },
+       body: JSON.stringify({
+         model: this.model,
+         input: {
+           text: text,
+           voice: this.voice,
+           language_type: this.languageType,
+           format: this.format,
+           sample_rate: this.sampleRate,
+         },
+       }),
+     }
+     */
+    try {
+      console.log('step2')
+    } catch(err) {
+      console.error(err)
+      await Taro.showToast({ title: '上传失败', icon: 'error' })
+    }
+  }
+
+  /**
+   * step 3：再调千问最后合成视频
+   * @param voiceId
+   * @param publicImagUrl
+   */
+  const generateVideo = async (voiceId: string, publicImagUrl: string) => {
+    console.log('step3')
+    /**
+     * image: publicImagUrl
+     * audio: todo, step 2生成的音频微信云公网地址
+     *
+     */
+    console.log('text: ' + blessingText + ' voiceId: ' + voiceId + ' imageUrl: ' + publicImagUrl);
+    const synthesisResponse = await Taro.request({
+      url: 'https://dashscope.aliyuncs.com/api/v1/services/aigc/video-generation/video-synthesis',
+      method: 'POST',
+      data: {
+        model: 'wan2.6-i2v-flash',
+        input: {
+          // prompt: '',
+          image_url: publicImagUrl,
+          audio_url: '',
+          url: 'audioUrl',
+          language_hints: ['zh']
+        }
+      },
+      header: {
+        'Content-type': 'application/json',
+        'Authorization': 'Bearer sk-b9e99c3d10504180bea3ef1edc4989af',
+        'X-DashScope-Async': true
+      },
+    })
+    console.log('请求成功 res: ' + synthesisResponse.statusCode);
+    console.log('res: ' + JSON.stringify(synthesisResponse));
+    console.log('voice_id: ' + synthesisResponse.data.output.task_id);
+
+    // 持久化合成的任务id
+    Taro.cloud.database().collection('user_task').add({
+      data: {
+        user_id: '123456',
+        task_id: synthesisResponse.data.output.task_id,
+        task_type: 'synthesis',
+        request_id: synthesisResponse.data.output.request_id,
+        gmt_create: Date.now()
+      }
+    });
+  }
 
   const handleGenerate = useCallback(async () => {
     if (currentFiles.length === 0) {
@@ -41,115 +174,14 @@ const VideoCreator: React.FC = () => {
       const ext = '.' + imageUrl.split('.').pop();
       const cloudPath = `uploads/images/${Date.now()}-${Math.floor(Math.random() * 1000)}${ext}`;
 
-      // 上传图片到微信云
-      Taro.cloud.uploadFile({
-        cloudPath: cloudPath,
-        filePath: imageUrl,
-        success: (response) => {
-          Taro.showToast({ title: '上传成功', icon: 'success' });
-          setTimeout(() => setDuration(0), 1000);
-
-          const res = Taro.cloud.getTempFileURL({
-            fileList: [response.fileID]
-          }).then((res) => {
-            const publicImagUrl = res.fileList[0].tempFileURL;
-            console.log('uploaded public image url: ' + publicImagUrl);
-
-            // 查询当前用户的voice_id
-            const voiceId = Taro.cloud.database().collection('user_voice')
-              .where({
-                'user_id': '123456'
-              }).get().then((res) => {
-                const voiceId = res.data[0].voice_id;
-                console.log('voiceId: ' + voiceId);
-
-                // step 2：根据祝福文本+之前的音色，调用大模型合成声音
-                // https://help.aliyun.com/zh/model-studio/cosyvoice-clone-api
-                /**
-                 * input:
-                 *  voice_id: voiceId
-                 *  text:  blessingText
-                 *
-                 * ouput:
-                 *  audioPublicUrl
-                 *
-                 * API: https://dashscope.aliyuncs.com/api/v1/services/aigc/text2audio/text-to-audio
-                 *         {
-                            method: 'POST',
-                            headers: {
-                                'Authorization': `Bearer ${this.apiKey}`,
-                                'Content-Type': 'application/json',
-                             },
-                            body: JSON.stringify({
-                             model: this.model,
-                             input: {
-                                  text: text,
-                                  voice: this.voice,
-                                  language_type: this.languageType,
-                                  format: this.format,
-                                  sample_rate: this.sampleRate,
-                                 },
-                            }),
-                            }
-                 */
-
-
-
-                // step 3：再调千问最后合成
-                /**
-                 * image: publicImagUrl
-                 * audio: todo, step 2生成的音频微信云公网地址
-                 *
-                 */
-                console.log('text: ' + blessingText + ' voiceId: ' + voiceId + ' imageUrl: ' + publicImagUrl);
-                const synthesisResponse = Taro.request({
-                  url: 'https://dashscope.aliyuncs.com/api/v1/services/aigc/video-generation/video-synthesis',
-                  method: 'POST',
-                  data: {
-                    model: 'wan2.6-i2v-flash',
-                    input: {
-                      // prompt: '',
-                      image_url: publicImagUrl,
-                      audio_url: '',
-                      url: 'audioUrl',
-                      language_hints: ['zh']
-                    }
-                  },
-                  header: {
-                    'Content-type': 'application/json',
-                    'Authorization': 'Bearer sk-b9e99c3d10504180bea3ef1edc4989af',
-                    'X-DashScope-Async': true
-                  },
-                  success: (res) => {
-                    console.log('请求成功 res: ' + res.statusCode);
-                    console.log('res: ' + JSON.stringify(res));
-                    console.log('voice_id: ' + res.data.output.task_id);
-
-                    // 持久化合成的任务id
-                    Taro.cloud.database().collection('user_task').add({
-                      data: {
-                        user_id: '123456',
-                        task_id: res.data.output.task_id,
-                        task_type: 'synthesis',
-                        request_id: res.data.output.request_id,
-                        gmt_create: Date.now()
-                      }
-                    });
-                  }
-                })
-
-
-              });
-
-          });
-
-        },
-        fail: err => {
-          console.log(err);
-        }
-      });
-
-      await new Promise(resolve => setTimeout(resolve, 2000));
+      // 获取 voiceId
+      const voiceId = await fetchVoiceId()
+      // step 1
+      const publicImagUrl = await uploadImageToWxCloud(cloudPath, imageUrl)
+      // step 2
+      await generateAudio()
+      // step 3
+      await generateVideo(voiceId, publicImagUrl)
 
       Taro.hideLoading();
       Taro.showModal({
