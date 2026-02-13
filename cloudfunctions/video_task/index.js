7b6adf3b16c9139169b0a1d161c1db37d0f1cb48
liuxin16 <liuxin16@liuxin16s-MacBook-Pro.local>
2026-02-13 16:46:19 +0800

    Michael.liuxin: add a unimplement faas function video_task

---
+++ cloudfunctions/video_task/index.js
@@ -0,0 +1,19 @@
+const cloud = require('wx-server-sdk')
+const dayjs = require('dayjs')
+
+// 初始化 cloud
+cloud.init({
+  // API 调用都保持和云函数当前所在环境一致
+  env: cloud.DYNAMIC_CURRENT_ENV
+})
+
+const db = cloud.database()
+const userInfoCollection = db.collection('user_info')
+
+exports.main = async (event, context) => {
+  // 获取 WX Context (微信调用上下文)，包括 OPENID、APPID、及 UNIONID（需满足 UNIONID 获取条件）等信息
+  const wxContext = cloud.getWXContext()
+
+
+}
+
